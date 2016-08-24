607ddc1842bc520a848eaaa524f6bc154ed99e24
Anthony Federico <dephoona@gmail.com>
2016-08-23 22:31:17 -0400

    Update DataExtractors.py

--- DataExtractors.py
+++ DataExtractors.py
@@ -2,6 +2,57 @@
 #             Twitter              #
 ####################################
 
+from twitter import Twitter as TWTR, OAuth
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import json
+import re
+import time
+
+# Credentials to access Twitter API 
+# ACCESS_TOKEN    = 'XXXXXXXXXXXXXXXXXXXXXXX'
+# ACCESS_SECRET   = 'XXXXXXXXXXXXXXXXXXXXXXX'
+# CONSUMER_KEY    = 'XXXXXXXXXXXXXXXXXXXXXXX'
+# CONSUMER_SECRET = 'XXXXXXXXXXXXXXXXXXXXXXX'
+
+# Initiate the connection to Twitter Streaming API
+# auth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+# Credentials = TWTR(auth=auth)
+
+def Twitter(Credentials, Query, Amount, Iterations = 1, Rest = 0): # Max 30 tweets per iteration, rest between each iteration
+
+    def Twitter_Stream(Query, Amount, Storage):
+        Iterator = Credentials.search.tweets(q=Query, result_type='recent', lang='en', count=Amount)
+        for Tweet in Iterator["statuses"]:
+            Storage[json.dumps(Tweet['text'])] = None
+    
+    def Twitter_Filter(text):
+        text = text.lower()
+        text = re.sub("[0-9]+", "number", text)
+        text = re.sub("rt", "", text)
+        text = text.replace('$', '@')
+        text = text.replace('\n', '@')
+        text = re.sub("@[^\s]+", "", text)
+        text = re.sub("(http|https)://[^\s]*", "", text)
+        text = re.sub("[^\s]+@[^\s]+", "", text)
+        return text
+    
+    def Twitter_Sentiment(Tweets):
+        SentimentScores = []
+        for Tweet in Tweets:
+            Tweet = Twitter_Filter(Tweet)
+            print Tweet
+            Score = SentimentIntensityAnalyzer().polarity_scores(Tweet)['compound']
+            if Score != 0:
+                SentimentScores.append(Score)
+        return round(sum(SentimentScores)/len(SentimentScores),3)
+    
+    Tweets = {}
+    Loops = 1
+    while Loops > 0:
+        Twitter_Stream(Query, Amount, Tweets)
+        time.sleep(0)
+        Loops -= 1
+    return Twitter_Sentiment(Tweets)
 
 ####################################
 #           StockTwits             #
@@ -45,14 +96,12 @@ def StockTwits(Ticker, Iterations = 1, Rest = 0): # Max 30 tweets per iteration,
         text = re.sub("[^\s]+@[^\s]+", "", text)
         return text
         
-    # Stream Twits    
     Twits = []
     while Iterations > 0:
         Twits += StockTwits_Stream(Ticker)
         time.sleep(Rest)
         Iterations -= 1
     
-    # Calculate sentiment scores
     SentimentScores = []
     for Twit in Twits:
         Support = Twit['Support']

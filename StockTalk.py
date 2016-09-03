1a8e7d4bfed2070581c33ae7f520664e0c532baa
Anthony Federico <dephoona@gmail.com>
2016-09-03 11:53:43 -0400

    Update StockTalk.py

--- StockTalk.py
+++ StockTalk.py
@@ -4,7 +4,7 @@ from DataExtractors import Twitter, StockTwits, GoogleNews
 # Request 100 (max) tweets from Twitter each iteration
 # Input rest (sec) between each iteration
 # Edit API credentials in DataExtractors.py file
-TW_Sentiment = Twitter(Query = '$TSLA', Iterations = 1, Rest = 0)
+TW_Sentiment = Twitter(Query = '$TSLA', Iterations = 5, Rest = 60)
 
 # Request 30 (max) twits from StockTwiters each iteration
 # Input rest (sec) between each iteration
@@ -12,7 +12,7 @@ ST_Sentiment = StockTwits(Ticker = 'TSLA', Iterations = 1, Rest = 0)
 
 # Request 10 (limit) pages of news headlines from Google News
 # Input rest (sec) between each page
-GN_Sentiment = GoogleNews('Tesla Motors', Pages = 1, Rest = 0)
+GN_Sentiment = GoogleNews('Tesla Motors', Pages = 5, Rest = 10)
 
 outfile = open('SocialSentimentLog.txt', 'a')
 outfile.write(time.strftime("%m/%d/%Y %H:%M ")+

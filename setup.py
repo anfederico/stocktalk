77eb88292bff16bc680ed06e62f113bc48e73574
Anthony Federico <dephoona@gmail.com>
2016-10-19 10:55:40 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from distutils.core import setup
-setup(
-  name = 'Stocktalk',
-  packages = ['Stocktalk'],
-  version = '1.0',
-  description = 'Data collection toolkit for social media analytics',
-  author = 'Anthony Federico',
-  author_email = 'dephoona@gmail.com',
-  url = 'https://github.com/anfederico/Stocktalk',
-  download_url = 'https://github.com/anfederico/Stocktalk/tarball/v0.1',
-  keywords = ['twitter', 'datamining', 'stock', 'analytics'],
-  license='MIT',
-  classifiers = [],
-)
+
+setup(name='stocktalk',
+      version='v0.1',
+      description='Data collection toolkit for social media analytics',
+      author='Anthony Federico',
+      author_email='dephoona@gmail.com',
+      url='https://github.com/anfederico/Stocktalk/tarball/v0.1',
+      packages=['stocktalk'],
+     )

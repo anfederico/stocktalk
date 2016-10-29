24bb02b373867a2c378e393b01dff509a63a0b1a
Anthony Federico <dephoona@gmail.com>
2016-10-28 22:17:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='stocktalk',
-      version='v0.5',
+      version='v0.6',
       description='Data collection toolkit for social media analytics',
       author='Anthony Federico',
       author_email='dephoona@gmail.com',

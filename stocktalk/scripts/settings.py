046649bd30cafc5e4f088bc5044d272b99274e98
Anthony Federico <anfederico@users.noreply.github.com>
2018-01-11 16:36:48 -0500

    Create settings.py

---
+++ stocktalk/scripts/settings.py
@@ -0,0 +1,13 @@
+# Mongo
+mongo_server = 'ds254236.mlab.com'
+mongo_id     =  54236
+mongo_client = 'stocktalk'
+mongo_user   = 'username'
+mongo_pass   = 'password'
+
+# Twitter
+api_key             = ''
+api_secret          = ''
+access_token        = ''
+access_token_secret = ''
+credentials = [api_key, api_secret, access_token, access_token_secret]

55e950ff943152874cdf8da3e3933418eb0b0b55
Anthony Federico <anfederico@users.noreply.github.com>
2018-01-11 17:08:19 -0500

    Update settings.py

--- stocktalk/scripts/settings.py
+++ stocktalk/scripts/settings.py
@@ -1,9 +1,9 @@
 # Mongo
-mongo_server = 'ds254236.mlab.com'
-mongo_id     =  54236
-mongo_client = 'stocktalk'
-mongo_user   = 'username'
-mongo_pass   = 'password'
+mongo_server = 'ds254236.mlab.com' # Find this in your deployment settings
+mongo_id     =  54236              # Find this in your deployment settings
+mongo_client = 'stocktalk'         # The name of your deployment
+mongo_user   = 'username'          # The username you added as a user to your deployment
+mongo_pass   = 'password'          # The password you made for that user
 
 # Twitter
 api_key             = ''

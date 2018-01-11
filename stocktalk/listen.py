02c15eaf69bc54395115d4b22abdb90c8ebb30cb
Anthony Federico <anfederico@users.noreply.github.com>
2018-01-11 17:06:11 -0500

    Update listen.py

--- stocktalk/listen.py
+++ stocktalk/listen.py
@@ -11,8 +11,8 @@ queries = {'ETH': ['ETH', 'Ethereum'],
 # Aggregate volume and sentiment every 15 minutes
 refresh = 15*60
 
-streaming.stream(settings.credentials, 
-                 queries, 
-                 refresh, 
-                 sentiment=True, 
-                 debug=True)
+streaming.streamer(settings.credentials, 
+                   queries, 
+                   refresh, 
+                   sentiment=True, 
+                   debug=True)

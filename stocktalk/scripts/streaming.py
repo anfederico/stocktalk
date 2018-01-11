42ba00cd41d8e937365e4bb2ef7867546d26d03e
Anthony Federico <anfederico@users.noreply.github.com>
2018-01-11 17:05:22 -0500

    Update streaming.py

--- stocktalk/scripts/streaming.py
+++ stocktalk/scripts/streaming.py
@@ -128,7 +128,7 @@ class Listener(tweepy.StreamListener):
  
 # Streaming --------------------------------------------------
 
-def streaming(credentials, queries, refresh, sentiment=False, debug=False):
+def stream(credentials, queries, refresh, sentiment=False, debug=False):
     keywords = [i for j in queries.values() for i in j]
 
     # User Error Checks
